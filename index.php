d7149f5ce703965cbd1004e4d2466f8528dd18dc
flooktos <fuflook@hotmail.com>
2017-03-20 20:56:46 +0700

    add bootstrap file
    เพิ่มไฟล์ต่างๆที่ต้องในโปรเจค

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="pic/favicon.ico">
+
+    <title>ระบบบริหารอู่ซ่อมรถ - บริษัท เนตรวชิรกุล จำกัด</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Bootstrap theme -->
+    <link href="css/bootstrap-theme.min.css" rel="stylesheet">
+    
+    <!-- Custom styles for this template -->
+    <link href="theme.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+
+    <!-- Fixed navbar -->
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a  class="navbar-brand" href="#">ระบบบริหารอู่ซ่อมรถ</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li><a class="active" href="#">หน้าแรก</a></li>
+            <li><a href="#A">กรอกข้อมูล</a></li>
+            <li><a href="#B">ประเมินการซ่อม</a></li>
+            <li><a href="#C">ซ่อม</a></li>
+            <li><a href="#D">ตรวจสอบคุณภาพ</a></li>
+            <li><a href="#E">อะไหล่</a></li>
+            <li class="dropdown">
+              <a href="#F" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">พิมพ์รายงาน <span class="caret"></span></a>
+              <ul class="dropdown-menu">
+                <li><a href="#F1">ใบรับรถ</a></li>
+                <li><a href="#F2">ใบสั่งอะไหล่</a></li>
+                <li><a href="#F3">ใบสัญญา</a></li>
+                <li><a href="#F4">ใบเสร็จ</a></li>
+              </ul>
+            </li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+
+    
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="js/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="js/jquery.min.js"><\/script>')</script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
